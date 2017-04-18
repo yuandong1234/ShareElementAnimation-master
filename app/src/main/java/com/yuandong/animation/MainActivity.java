67056d701823678c0abd8b056ff7a1aa569c6299
yuandong1234 <yuan_dong1992@163.com>
2017-04-18 17:51:20 +0800

    .

--- app/src/main/java/com/yuandong/animation/MainActivity.java
+++ app/src/main/java/com/yuandong/animation/MainActivity.java
@@ -2,12 +2,16 @@ package com.yuandong.animation;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.ListView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity{
     private ListView listView;
+    private ImageView iv_pic;
     private MyAdapter adapter;
     String url = "http://p4.so.qhimg.com/t010c102c7b029340d4.jpg";
     @Override
@@ -18,6 +22,11 @@ public class MainActivity extends AppCompatActivity{
     }
 
     private void initView(){
+        iv_pic=(ImageView)findViewById(R.id.iv_pic);
+        Glide.with(this)
+                .load(url)
+                .animate(R.anim.gilde_load_pic)
+                .into(iv_pic);
         listView=(ListView) findViewById(R.id.listView);
         ArrayList<String> urls=new ArrayList<>();
         for(int i=0;i<10;i++){
