9ece15bca97eb7d62917f98cbf73629d61c22123
yuandong1234 <yuan_dong1992@163.com>
2017-04-18 16:52:28 +0800

    .

---
+++ app/src/main/java/com/yuandong/animation/OtherActivity.java
@@ -0,0 +1,30 @@
+package com.yuandong.animation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class OtherActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageView iv_pic;
+    String internetUrl = "http://p4.so.qhimg.com/t010c102c7b029340d4.jpg";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+        iv_pic = (ImageView) findViewById(R.id.iv_pic);
+        iv_pic.setOnClickListener(this);
+        Glide.with(this)
+                .load(internetUrl)
+                .into(iv_pic);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        onBackPressed();
+    }
+}
